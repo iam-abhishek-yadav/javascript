040e9d41fda073b3a88210094c5057b9e0bae99f
iam-abhishek-yadav <abhishekyadav.recs@gmail.com>
2023-12-28 21:03:13 +0530

    add: adding array algorithms

---
+++ algorithms/03-arrays/spiralPrintAnArray.js
@@ -0,0 +1,43 @@
+// Q-54 in leetcode
+var spiralOrder = function(matrix) {
+    var result = [];
+    let [left, right, top, bottom] = [0, matrix[0].length-1, 0, matrix.length-1];
+
+    while(top <= bottom && left <= right){
+        for(var i=left;i<=right;i++){
+            result.push(matrix[top][i]);
+        }
+        top++;
+
+        for(var i=top;i<=bottom;i++){
+            result.push(matrix[i][right]);
+        }
+        right--;
+
+        if(top <= bottom){
+            for(var i=right;i>=left;i--){
+                result.push(matrix[bottom][i]);
+            }
+            bottom--;
+        }
+
+        if(left <= right){
+            for(var i=bottom;i>=top;i--){
+                result.push(matrix[i][left]);
+            }
+            left++;
+        }
+    }
+
+    return result;
+};
+
+var inputMatrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+
+var resultArray = spiralOrder(inputMatrix);
+
+console.log("Spiral Order:", resultArray);
