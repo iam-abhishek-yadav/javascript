59454943b532331701f5809731398f345f699ce3
iam-abhishek-yadav <abhrecscs17@gmail.com>
2024-03-17 19:47:44 +0530

    Add: striver79:16-20

---
+++ striver79/20_intersectionOfTwoLinkedList.js
@@ -0,0 +1,9 @@
+var getIntersectionNode = function (headA, headB) {
+    let a = headA;
+    let b = headB;
+    while (a != b) {
+        a = a ? a.next : headB;
+        b = b ? b.next : headA;
+    }
+    return a;
+};
