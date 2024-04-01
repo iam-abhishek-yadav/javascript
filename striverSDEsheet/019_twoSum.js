a27717c1b9ff5dc8b29ce66648055e7322eaf7f6
iam-abhishek-yadav <abhrecscs17@gmail.com>
2024-04-01 14:49:42 +0530

    Add: striverSDE:016-020

---
+++ striverSDEsheet/019_twoSum.js
@@ -0,0 +1,11 @@
+var twoSum = function(nums, target) {
+    const map = new Map();
+    for(let i=0;i<nums.length;i++){
+        const rem = target-nums[i];
+        if(map.has(rem)){
+            return [map.get(rem), i];
+        }
+        map.set(nums[i], i);
+    }
+    return [];
+};
